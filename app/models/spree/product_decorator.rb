bb5c8fdead3f74d924c7d57686272d97a8b7e04d
chinoxchen <chienfu.udp@gmail.com>
2022-09-22 02:09:12 -0300

    fix: autocomplete store filter

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -41,12 +41,15 @@ module Spree::ProductDecorator
           available: true
         }
 
-          # filtramos solo para cada tienda, excepto lomi.cl (marketplace)
-          res[:store_ids] = [current_store_id] if current_store_id.present? && current_store_id != 1
-
-          # filtramos lomiexpress.cl para el marketplace
-          res[:store_ids] = { not: [2] } if current_store_id.present? && current_store_id != 2
-
+          if current_store_id.present? && current_store_id != 1 && current_store_id != 2
+            res[:_and] = [{store_ids: [current_store_id]}, {store_ids: { not: [2] }}]
+          else
+            # filtramos solo para cada tienda, excepto lomi.cl (marketplace)
+            res[:store_ids] = [current_store_id] if current_store_id.present? && current_store_id != 1
+  
+            # filtramos lomiexpress.cl para el marketplace
+            res[:store_ids] = { not: [2] } if current_store_id.present? && current_store_id != 2
+          end
         res
       end
 
